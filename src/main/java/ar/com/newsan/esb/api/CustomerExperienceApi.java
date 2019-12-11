faa44995482d28f4062b2be11469fba39f82687d
Sergio Platas <splatas@redhat.com>
2019-12-11 11:53:50 -0300

    API services.

--- src/main/java/ar/com/newsan/esb/api/CustomerExperienceApi.java
+++ src/main/java/ar/com/newsan/esb/api/CustomerExperienceApi.java
@@ -10,17 +10,20 @@ import javax.ws.rs.QueryParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.newsan.esb.routes.NewsanRouteBuilder;
-import ar.com.newsan.esb.security.JWTSecured;
 import ar.com.newsan.esb.utils.StandardResponse;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-//@Api(description = "Customer Experience API")
+@Api("Stock Service API")
 //@Path("/custexp")
+@RestController
 public class CustomerExperienceApi {
 
     private static Logger LOG = LoggerFactory.getLogger(CustomerExperienceApi.class);
@@ -28,21 +31,29 @@ public class CustomerExperienceApi {
     @Autowired
     private NewsanRouteBuilder routeBuilder;
 
+	//    @GET
+	//    @Path("/product/{sku}/stock")
+	//    @Produces({ "application/json" })
+	//    @ApiOperation(value = "find a product stock in EBS", notes = "Consulta el stock de un producto en EBS", response = StandardResponse.class, tags={ "products",  })
+	//    @ApiResponses(value = {
+	//            @ApiResponse(code = 200, message = "Successful Operation", response = StandardResponse.class),
+	//            @ApiResponse(code = 404, message = "Product not Found", response = StandardResponse.class),
+	//            @ApiResponse(code = 500, message = "Internal Server Error", response = StandardResponse.class) })
+    //    @JWTSecured
+    
     @GET
-    @Path("/product/{sku}/stock")
+    @RequestMapping("/product/{sku}/stock")
     @Produces({ "application/json" })
     @ApiOperation(value = "find a product stock in EBS", notes = "Consulta el stock de un producto en EBS", response = StandardResponse.class, tags={ "products",  })
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successful Operation", response = StandardResponse.class),
-            @ApiResponse(code = 404, message = "Product not Found", response = StandardResponse.class),
-            @ApiResponse(code = 500, message = "Internal Server Error", response = StandardResponse.class) })
-    //@JWTSecured
+	    @ApiResponses(value = {
+	            @ApiResponse(code = 200, message = "Successful Operation", response = StandardResponse.class),
+	            @ApiResponse(code = 404, message = "Product not Found", response = StandardResponse.class),
+	            @ApiResponse(code = 500, message = "Internal Server Error", response = StandardResponse.class) })
     public StandardResponse findProductStock(@QueryParam("organization") @NotNull String organization,
                                              @QueryParam("subinventory") @NotNull String subinventory,
                                              @PathParam("sku") @ApiParam("pass a product sku for looking up specific product") String sku) {
-        LOG.info("[CustomerExperienceApi.findProductStock]: starts with: sku=" + sku  + ", organization= " + organization + ", subinventory=" + subinventory);
 
-        //routeBuilder.getRouteCollection().
+    	LOG.info("[CustomerExperienceApi.findProductStock]: starts with: sku=" + sku  + ", organization= " + organization + ", subinventory=" + subinventory);
         
     	return StandardResponse.ok("findProductStock");
     }
